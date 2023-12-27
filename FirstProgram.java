8610ad7ad4f5296ef73338f9fc44e56b5d948bae
LamaGokulakrishnan <83019892+lamagokulakrishnan@users.noreply.github.com>
2023-12-27 11:06:35 +0530

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,17 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        String name = "GOKULAKRISHNAN";
+
+        long regNo = 4011;
+
+        String department = "CSE";
+
+        String collegeName = "Al-Ameen Engineering College";
+
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + regNo);
+        System.out.println("Department: " + department);
+        System.out.println("College Name: " + collegeName);
+    }
+}
